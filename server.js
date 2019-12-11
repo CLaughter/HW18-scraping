6a5b366f96862c4cf9751cf448ac11ba35c5eb83
Chris Laughter <chrislaugh@gmail.com>
2019-12-11 16:54:58 -0700

    initial files

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import { Mongoose } from "mongoose";
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb:localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 Mongoose.connect(MONGODB_URI);
