930b7a14acdd0860da93d722b88b0cc689884f61
Chris Laughter <chrislaugh@gmail.com>
2019-12-15 16:55:36 -0700

    initial files

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+require("dotenv").config();
 
+// DB connection
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
+
+var express = require("express");
+var app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+var exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on PORT: " + port
+  );
+});
+
+module.exports = app;
